079ec9da8d834aad39861818581601f9b84158d2
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-03 05:02:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     alan_kay
+     programmer_hash[:alan_kay][:known_for]= "GUI"
+     programmer_has
      
 end
 
