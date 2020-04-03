e117b2e9124c5301b48294b9c7c69e16901e5278
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-03 04:55:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,11 +69,7 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{    :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        },
-        :grace_hopper => {
+ 	{      :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
@@ -88,7 +84,8 @@ def adding_matz
      }
 
    
-     programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto]={:known_for=>"Ruby",:languages=>["LISP","C"]}
+  programmer_hash
      
 end
 
